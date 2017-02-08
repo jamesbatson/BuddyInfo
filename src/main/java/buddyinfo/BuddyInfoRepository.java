144016c0353539ebace71557f12873217d092d5d
Adam J Batson <adamjbatson@gmail.com>
2017-02-07 19:15:13 -0500

    IT WORKS!!!

--- src/main/java/buddyinfo/BuddyInfoRepository.java
+++ src/main/java/buddyinfo/BuddyInfoRepository.java
@@ -1,12 +1,15 @@
 package buddyinfo;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import java.util.List;
 
 /**
  * Created by adambatson on 2/7/2017.
  */
+
+@RepositoryRestResource(collectionResourceRel = "buddies", path = "buddies")
 public interface BuddyInfoRepository extends CrudRepository<BuddyInfo, Long> {
     //List<BuddyInfo> findAll();
 }

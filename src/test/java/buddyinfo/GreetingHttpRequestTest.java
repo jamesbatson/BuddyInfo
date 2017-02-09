672563474e4945f18a16d9e95d7e5f3b1cd04029
Adam J Batson <adamjbatson@gmail.com>
2017-02-09 09:29:01 -0500

    More unit testing

---
+++ src/test/java/buddyinfo/GreetingHttpRequestTest.java
@@ -0,0 +1,35 @@
+package buddyinfo;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.embedded.LocalServerPort;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+public class GreetingHttpRequestTest {
+
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    public void greetingShouldReturnDefaultMessage() throws Exception {
+        assertThat(this.restTemplate.getForObject("http://localhost:" + port + "/greeting",
+                String.class)).contains("Hello, World");
+    }
+
+    @Test
+    public void greetingShouldReturnNamedMessage() throws Exception {
+        assertThat(this.restTemplate.getForObject("http://localhost:" + port + "/greeting?name=adam",
+                String.class)).contains("Hello, adam");
+    }
+}
